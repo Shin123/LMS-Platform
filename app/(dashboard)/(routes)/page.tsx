64081dad9a0ff55aafa8f6f9b01285dc36266259
Shin123 <vanvietkhtn@gmail.com>
2024-01-15 17:09:51 +0700

    implement Sign-In, Sign-Up with clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <p className='text-3xl font-medium text-sky-700'>Hello</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   )
 }
