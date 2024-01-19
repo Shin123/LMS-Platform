5c1e889c485e65bf52cc38221958dc8fbddfc9fb
Shin123 <vanvietkhtn@gmail.com>
2024-01-19 16:03:38 +0700

    Add AttachmentForm component

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -0,0 +1,94 @@
+'use client'
+
+import axios from 'axios'
+import * as z from 'zod'
+
+import { useRouter } from 'next/navigation'
+
+import FileUpLoad from '@/components/file-upload'
+import { Button } from '@/components/ui/button'
+import { Attachment, Course } from '@prisma/client'
+import { File, ImageIcon, Pencil, PlusCircle } from 'lucide-react'
+import Image from 'next/image'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] }
+  courseId: string
+}
+
+const formSchema = z.object({
+  url: z.string().min(1)
+})
+
+const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) => {
+  const [isEditing, setIsEditing] = useState(false)
+
+  const toggleEdit = () => setIsEditing((current) => !current)
+
+  const router = useRouter()
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values)
+      toast.success('Course updated')
+      toggleEdit()
+      router.refresh()
+    } catch {
+      toast.error('Something went wrong')
+    }
+  }
+
+  return (
+    <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+      <div className='font-medium flex items-center justify-between'>
+        Course attachments
+        <Button onClick={toggleEdit} variant='ghost'>
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <PlusCircle className='h-4 w-4 mr-2' />
+              Add a file
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className='text-sm mt-2 text-slate-500 italic'>No attachments yet</p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className='space-y-2'>
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className='flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md'
+                >
+                  <File className='h-4 w-4 mr-2 flex-shrink-0' />
+                  <p className='text-xs line-clamp-1'>{attachment.name}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpLoad
+            endpoint='courseAttachment'
+            onChange={(url) => {
+              onSubmit({ url: url })
+            }}
+          />
+          <div className='text-xs text-muted-foreground mt-4'>
+            Add anything your students might need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AttachmentForm
