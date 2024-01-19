6065210d23241bd8d3bda6bf7c131bdc56be4277
Shin123 <vanvietkhtn@gmail.com>
2024-01-20 00:11:25 +0700

    feat: done func drag and drop chapter for reorder

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -10,12 +10,13 @@ import { useRouter } from 'next/navigation'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { Pencil, PlusCircle } from 'lucide-react'
+import { Loader2, Pencil, PlusCircle } from 'lucide-react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 import { cn } from '@/lib/utils'
 import { Textarea } from '@/components/ui/textarea'
 import { Chapter, Course } from '@prisma/client'
+import ChaptersList from './chapters-list'
 
 interface ChaptersFormProps {
   initialData: Course & { chapters: Chapter[] }
@@ -50,8 +51,32 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   }
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true)
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData
+      })
+      toast.success('Chapters reordered')
+      router.refresh()
+    } catch {
+      toast.error('Something went wrong')
+    } finally {
+      setIsUpdating(false)
+    }
+  }
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+  }
+
   return (
-    <div className='mt-6 border bg-slate-100 rounded-md p-4'>
+    <div className='relative mt-6 border bg-slate-100 rounded-md p-4'>
+      {isUpdating && (
+        <div className='absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center'>
+          <Loader2 className='animate-spin h-6 w-6 text-sky-700 ' />
+        </div>
+      )}
       <div className='font-medium flex items-center justify-between'>
         Course chapters
         <Button onClick={toggleCreating} variant='ghost'>
@@ -90,6 +115,7 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
       {!isCreating && (
         <div className={cn('text-sm mt-2', !initialData.chapters.length && 'text-slate-500 italic')}>
           {!initialData.chapters.length && 'No chapters'}
+          <ChaptersList onEdit={onEdit} onReorder={onReorder} items={initialData.chapters || []} />
         </div>
       )}
       {!isCreating && (
