847810c75729278f6371f6cb68c36f96f3875c2c
Shin123 <vanvietkhtn@gmail.com>
2024-01-23 15:55:47 +0700

    feat: add func publish and unpublish chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -18,6 +18,27 @@ interface ChapterActionsProps {
 const ChapterActions = ({ disabled, courseId, chapterId, isPublished }: ChapterActionsProps) => {
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true)
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`)
+        toast.success('Chapter unpublished')
+      } else {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`)
+        toast.success('Chapter published')
+      }
+
+      router.refresh()
+    } catch {
+      toast.error('Something went wrong')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   const onDelete = async () => {
     try {
       setIsLoading(true)
@@ -35,7 +56,7 @@ const ChapterActions = ({ disabled, courseId, chapterId, isPublished }: ChapterA
   }
   return (
     <div className='flex items-center gap-x-2'>
-      <Button onClick={() => {}} disabled={disabled || isLoading} variant='outline' size='sm'>
+      <Button onClick={onClick} disabled={disabled || isLoading} variant='outline' size='sm'>
         {isPublished ? 'Unpublish' : 'Publish'}
       </Button>
       <ConfirmModal onConfirm={onDelete}>
