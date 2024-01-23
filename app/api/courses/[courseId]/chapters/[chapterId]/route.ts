4a95b0e2c36db50ae424f794d329be95e99b5d42
Shin123 <vanvietkhtn@gmail.com>
2024-01-23 15:30:55 +0700

    api: add func DELETE chapterId

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -5,6 +5,85 @@ import Mux from '@mux/mux-node'
 
 const { Video } = new Mux(process.env.MUX_TOKEN_ID!, process.env.MUX_TOKEN_SECRET!)
 
+export async function DELETE(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
+  try {
+    const { userId } = auth()
+    const { courseId, chapterId } = params
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId
+      }
+    })
+
+    if (!ownCourse) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        courseId
+      }
+    })
+
+    if (!chapter) {
+      return new NextResponse('Not Found', { status: 404 })
+    }
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId
+        }
+      })
+
+      if (existingMuxData) {
+        await Video.Assets.del(existingMuxData.assetId)
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id
+          }
+        })
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: {
+        id: chapterId
+      }
+    })
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: courseId,
+        isPublished: true
+      }
+    })
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: courseId
+        },
+        data: {
+          isPublished: false
+        }
+      })
+    }
+
+    return NextResponse.json(deletedChapter)
+  } catch (error) {
+    console.log('[CHAPTER_ID_DELETE', error)
+    return new NextResponse('Internal Server Error', { status: 500 })
+  }
+}
+
 export async function PATCH(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
   try {
     const { userId } = auth()
