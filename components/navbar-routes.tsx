bf2ca5222f3211a9dbe75bee67b5148740eb0ba7
Shin123 <vanvietkhtn@gmail.com>
2024-01-25 11:00:59 +0700

    Add seachInput component

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,31 +5,40 @@ import { UserButton } from '@clerk/nextjs'
 import { LogOut } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { SearchInput } from './search-input'
 
 const NavbarRoutes = () => {
   const pathName = usePathname()
 
   const isTeacherPage = pathName?.startsWith('/teacher')
   const isPlayerPage = pathName?.includes('/chapter')
+  const isSearchPage = pathName === '/search'
 
   return (
-    <div className='flex gap-x-2 ml-auto'>
-      {isTeacherPage || isPlayerPage ? (
-        <Link href='/'>
-          <Button size='sm' variant='ghost'>
-            <LogOut className='h-4 w-4 mr-2' />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href='/teacher/courses'>
-          <Button size='sm' variant='ghost'>
-            Teacher mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className='hidden md:block'>
+          <SearchInput />
+        </div>
       )}
-      <UserButton afterSignOutUrl='/' />
-    </div>
+      <div className='flex gap-x-2 ml-auto'>
+        {isTeacherPage || isPlayerPage ? (
+          <Link href='/'>
+            <Button size='sm' variant='ghost'>
+              <LogOut className='h-4 w-4 mr-2' />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href='/teacher/courses'>
+            <Button size='sm' variant='ghost'>
+              Teacher mode
+            </Button>
+          </Link>
+        )}
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </>
   )
 }
 
