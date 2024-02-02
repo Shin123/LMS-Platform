4045a5175e60ecd363ca39a1d65cc9ee8ea5a0b0
Shin123 <vanvietkhtn@gmail.com>
2024-02-02 15:28:57 +0700

    add cour-progress component and update variant success

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import { IconBadge } from './icon-badge'
 import { BookOpen } from 'lucide-react'
 import { formatPrice } from '@/lib/format'
+import { CourseProgress } from './course-progress'
 
 interface CourseCardProps {
   id: string
@@ -38,7 +39,7 @@ export const CourseCard = ({ id, title, imageUrl, chaptersLength, price, progres
           </div>
 
           {progress !== null ? (
-            <div>Todo: Progress component</div>
+            <CourseProgress variant={progress === 100 ? 'success' : 'default'} size='sm' value={progress} />
           ) : (
             <p className='text-md md:text-sm font-medium text-slate-700'>{formatPrice(price)}</p>
           )}
